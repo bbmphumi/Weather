b258a4bbcab366c944f22e0e3c9f2dc9a5e9e447
bbmphumi <bbmphumi@gmail.com>
2025-04-16 12:53:07 +0200

    starting

---
+++ src/app/home/weather.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({ providedIn: 'root' })
+export class WeatherService {
+  private apiKey = environment.apiKey;
+  private baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+  constructor(private http: HttpClient) {}
+
+  getWeather(city: string): Observable<any> {
+    const url = `${this.baseUrl}?q=${city}&appid=${this.apiKey}&units=metric`;
+    return this.http.get(url);
+  }
+}
